d8b45331ca006054f89a04ebaddccc5f46e1194c
Rauan Rosa <99094020+rauandesantana@users.noreply.github.com>
2023-11-07 20:42:55 -0300

    Estruturando Pagina Home

--- lib/pages/main/pages_home.dart
+++ lib/pages/main/pages_home.dart
@@ -5,8 +5,46 @@ class $PagesHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const String urlPrivacyPolicy = "https://google.com.br";
+
     return Scaffold(
-      body: Components.bodyGradient(child: Center()),
+      body: Components.bodyGradient(
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+
+
+                      // ------------------------------------------------------- XXXXX Field
+                      Components.textFormField(
+                        hintText: "Digite seu texto",
+                      ),
+
+
+
+
+
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // ----------------------------------------------------------------- Privacy Policy Button
+            Padding(
+              padding: const EdgeInsets.only(bottom: 35),
+              child: GestureDetector(
+                onTap: () => Services.launcherUrl.open(urlPrivacyPolicy),
+                child: Text(Languages.current.privacyPolicy),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
