90a90e15e671a31b1da66ee2afd03bcdf2a090bf
Rauan Rosa <99094020+rauandesantana@users.noreply.github.com>
2023-11-06 22:43:22 -0300

    Adicionando Animação Splash Screen

--- lib/pages/boot/pages_splash_screen.dart
+++ lib/pages/boot/pages_splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:provaflutter/import_collections.dart';
+import 'package:provaflutter/providers/providers_app_state.dart';
 
 class $PagesSplashScreen extends StatefulWidget {
   const $PagesSplashScreen({super.key});
@@ -7,31 +8,43 @@ class $PagesSplashScreen extends StatefulWidget {
   State<$PagesSplashScreen> createState() => _$PagesSplashScreenState();
 }
 
-class _$PagesSplashScreenState extends State<$PagesSplashScreen> {
+class _$PagesSplashScreenState extends State<$PagesSplashScreen> with TickerProviderStateMixin {
   final double _size = 180;
+
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 2),
+    vsync: this,
+  )..repeat(reverse: true);
+
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.elasticOut,
+  );
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    _controller.dispose();
+  }
 
   @override
   void initState() {
     super.initState();
-    // ------------------------------------------------------------------------- Loading Dependencies
-    Future.wait([
-      // ----------------------------------------------------------------------- List Dependencies
-      Future.delayed(const Duration(seconds: 2), () => "firebase"),
-      Future.delayed(const Duration(seconds: 1), () => "another"),
-      // Para Testar o Alerta de Erro ao Carregar
-      // Future.delayed(const Duration(seconds: 3), () => throw "errorAlertTest"),
-    ]).then((result) {
-      // ----------------------------------------------------------------------- Success
-      print(result);
-    }).catchError((error, stack) {
-      // ----------------------------------------------------------------------- Error
-      print(error);
-    }).whenComplete(() {
-      // ----------------------------------------------------------------------- Finally
-      print("finally");
-    });
+    final appState = context.read<ProvidersAppState>();
+    // ------------------------------------------------------------------------- Initialize
+    if (appState.isInitialized == false) {
+      appState.initialize([
+        // --------------------------------------------------------------------- List Dependencies
+        Future.delayed(const Duration(seconds: 2), () => "firebase"),
+        Future.delayed(const Duration(seconds: 1), () => "another"),
+        // Para Testar o Alerta de Erro ao Carregar
+        // Future.delayed(const Duration(seconds: 3), () => throw "errorAlertTest"),
+      ]);
+    } else {
+      appState.restart();
+    }
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,9 +63,12 @@ class _$PagesSplashScreenState extends State<$PagesSplashScreen> {
                 height: _size,
                 child: Padding(
                   padding: const EdgeInsets.all(40),
-                  child: Image.asset(
-                    Assets.images.logos.flutter,
-                    color: Theme.of(context).colorScheme.primary,
+                  child: RotationTransition(
+                    turns: _animation,
+                    child: Image.asset(
+                      Assets.images.logos.flutter,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
                   ),
                 ),
               )
