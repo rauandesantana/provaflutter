4e514a8aa82df9dad223f1ed8c3f88043fbd0024
Rauan Rosa <99094020+rauandesantana@users.noreply.github.com>
2023-11-08 21:55:50 -0300

    Excluindo Pasta de Teste

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:provaflutter/main.dart';
-
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const ProvaFlutter());
-
-  });
-}
